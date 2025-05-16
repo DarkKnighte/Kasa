4a32825690f3b94575480aa8bbb851a2d2de1f37
Gabriel Gambotti <ggambotti@uth.fr>
2025-05-16 16:11:07 +0200

    Scss + router installer

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from "react-router";
+import './index.scss'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
@@ -8,3 +9,9 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
