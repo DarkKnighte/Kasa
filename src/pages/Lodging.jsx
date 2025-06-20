c180ca3b5e5889ee4594c2c58e55fc60895649de
Gabriel Gambotti <ggambotti@uth.fr>
2025-06-20 15:42:42 +0200

    carousel V.09

--- src/pages/Lodging.jsx
+++ src/pages/Lodging.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams, Navigate } from 'react-router-dom';
-// import Carousel from '../components/Carousel'; // À importer si vous avez des données
+import Carousel from '../components/Carousel'; // À importer si vous avez des données
 // import Collapse from '../components/Collapse'; // À importer si vous avez des données
 import logements from '../../data.json'; // Exemple de source de données
 import './Lodging.scss'; // Assurez-vous que ce fichier SCSS existe ou créez-le
@@ -15,7 +15,8 @@ const Lodging = () => {
 
   return (
     <div className="lodging-page">
-        { /* @TODO: Ajouter le carousel. ⚠️ Ne pas afficher les flèches et la numérotation si il n'y a qu'une seule image. */ }
+        {/* Carousel pour afficher les images du logement */}
+        {logement.pictures && <Carousel pictures={logement.pictures} />}
         { /* @TODO: Ajouter les détails du logement (avec des composants personnalisées si voulues). */ }
         { /* @TODO: Ajouter la description et les équipements du logement via les collapses. */ }
     </div>
