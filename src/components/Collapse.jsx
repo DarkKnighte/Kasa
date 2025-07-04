d97ac9072ceef3b4b9b65f0649fe6029947dae99
Gabriel Gambotti <gabrielgambotti2006@gmail.com>
2025-07-04 14:32:59 +0200

    lodging page fini

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Collapse.scss';
+import arrow from '../assets/arrow_back.svg';
 
 const Collapse = ({ title, children }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,10 +13,7 @@ const Collapse = ({ title, children }) => {
     <div className={`collapse ${isOpen ? 'collapse--open' : ''}`}>
       <div className="collapse__header" onClick={toggleCollapse}>
         <h3 className="collapse__title">{title}</h3>
-        <span className={`collapse__icon ${isOpen ? 'collapse__icon--open' : ''}`}>
-          {/* Vous pouvez utiliser une icône SVG ou une image ici */}
-          ▼
-        </span>
+        <img src={arrow} alt="dérouler" className={`collapse__icon ${isOpen ? 'collapse__icon--open' : ''}`} />
       </div>
       {isOpen && <div className="collapse__content">{ children }</div>}
     </div>
